9470daab8b199603d78506f1716a5d026093b974
Devin <devin@blackhat.co.za>
2024-01-09 15:18:39 +0200

    ts boilerplate

---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.coins = void 0;
+exports.coins = ["btc", "eth", "xmr"];
---
+++ ts_src/index.ts
@@ -0,0 +1,1 @@
+export const coins = ['btc', 'eth', 'xmr'];
