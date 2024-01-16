0654a2e86d3c249e4a67e5c7cb541979d0e49f9d
Karl Buys <kbbuys@gmail.com>
2024-01-16 10:23:40 +0200

    feat: adds support for many eth JSON-RPC equivalent commands

--- ts_src/ethereum/index.ts
+++ ts_src/ethereum/index.ts
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers';
+import { Block, ethers } from 'ethers';
 
 class EthClient {
     // Properties
@@ -15,20 +15,7 @@ class EthClient {
         console.log(provider);
         this.apiKey = apiKey;
         this.provider = provider;
-        this.getBlock();
-    }
-  
-
-    // Method
-    // initConnector(TProvider: string): void {
-    //   console.log(`we are connecting to {provider}.`);
-    // }
-
-    async getBlockByNumber(number: number) {
-        // const provider = new ethers.AlchemyProvider(undefined, "VqDjBvWuSn2RjrjMERoTIRw0VKlkGRRT");
-        // const block = await provider.getBlock(hash);
-        //console.log(block);
-        let block = await this.provider.getBlock(19011632);
+        let block = this.getBlockByNumber(19011632);
         console.log(block);
     }
 
@@ -37,43 +24,59 @@ class EthClient {
     // **TODO** :
     //Returns the number of most recent block.
     async blockNumber() {
-
+        let blockNumber = await this.provider.getBlockNumber();
+        return blockNumber;
     }
 
     // **TODO** :
     //Returns the balance of the account of a given address.
-    async getBalance() {
-
+    async getBalance(address: string) {
+        let balance = await this.provider.getBalance(address);
+        return balance;
     }
 
     // **TODO** :
     //Returns the value from a storage position at a given address.
-    async getStorageAt() {
-
+    // address: Address to get storage from
+    // slotPosition: The position in the storage to get the value from (in hex)
+    // Either the hex value of a block number OR a block hash OR One of the following block tags:
+    // - pending - A sample next block built by the client on top of latest and containing the set of transactions usually taken from local mempool. Intuitively, you can think of these as blocks that have not been mined yet.
+    // - latest - The most recent block in the canonical chain observed by the client, this block may be re-orged out of the canonical chain even under healthy/normal conditions.
+    // - safe - The most recent crypto-economically secure block, cannot be re-orged outside of manual intervention driven by community coordination. Intuitively, this block is “unlikely” to be re-orged.
+    // - finalized - The most recent crypto-economically secure block, that has been accepted by >2/3 of validators. Cannot be re-orged outside of manual intervention driven by community coordination. Intuitively, this block is very unlikely to be re-orged.
+    // - earliest - The lowest numbered block the client has available. Intuitively, you can think of this as the first block created.
+    async getStorageAt(address: string, slotPosition: string, blockTagOrNumberOrHash?: string) {
+        let storage = await this.provider.getStorage(address, slotPosition, blockTagOrNumberOrHash);
+        return storage;
     }
 
     // **TODO** :
-    //Returns the number of transactions sent from an address.
-    async getTransactionCount() {
+    // Returns the number of transactions sent from an address.
+    async getTransactionCount(address: string, blockTagOrNumberOrHash?: string) {
+        let transactionCount = await this.provider.getTransactionCount(address, blockTagOrNumberOrHash);
+        return transactionCount;
+    }
 
+    // Returns a Block by its number
+    // Duplicates the getBlock function, but we want to stick as closely to the original JSON-RPC commands
+    async getBlockByNumber(number: number) {
+        let block = await this.provider.getBlock(number);
+        return block;
     }
 
     // **TODO** :
-    //Returns information about a block based on its hash.
-    async getBlockByHash() {
-
-    }
-
-    // **TODO** :
-    //Returns information about a block based on its number.
-    async getBlockByNumber() {
-
+    // Returns information about a block based on its hash.
+    // Duplicates the getBlock function, but we want to stick as closely to the original JSON-RPC commands
+    async getBlockByHash(hash: string) {
+        let block = await this.provider.getBlock(hash);
+        return block;
     }
 
     // **TODO** :
     //Returns the information about a transaction requested by transaction hash.
-    async getTransactionByHash() {
-
+    async getTransactionByHash(hash: string) {
+        let transaction = await this.provider.getTransaction(hash);
+        return transaction;
     }
 
     // **TODO** :
@@ -108,7 +111,7 @@ class EthClient {
 
     // **TODO** :
     //Sends a raw transaction.
-    async sendRawTransaction( {
+    async sendRawTransaction() {
 
     } 
 
@@ -153,10 +156,6 @@ class EthClient {
     async compileSerpent() {
 
     }
-
-
-
-    
 }
 
 
@@ -169,6 +168,30 @@ console.log("Check this out");
 
 let ethClient = new EthClient("VqDjBvWuSn2RjrjMERoTIRw0VKlkGRRT");
 console.log(ethClient);
+
+// https://www.newline.co/@bespoyasov/how-to-use-fetch-with-typescript--a81ac257
+// Make the `request` function generic
+// to specify the return data type:
+function request<TResponse>(
+    url: string,
+    // `RequestInit` is a type for configuring 
+    // a `fetch` request. By default, an empty object.
+    config: RequestInit = {}
+     
+  // This function is async, it will return a Promise:
+  ): Promise<TResponse> {
+      
+    // Inside, we call the `fetch` function with 
+    // a URL and config given:
+    return fetch(url, config)
+      // When got a response call a `json` method on it
+      .then((response) => response.json())
+      // and return the result data.
+      .then((data) => data as TResponse);
+      
+      // We also can use some post-response
+      // data-transformations in the last `then` clause.
+  }
 
 /* List of JSON-RPC methods
 
