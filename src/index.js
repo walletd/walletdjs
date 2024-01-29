34b0c23d54d8b971f19581dae1cca17d1f15997c
Karl Buys <kbbuys@gmail.com>
2024-01-29 15:45:00 +0200

    build: re-add src file to version control for non-ts imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.coins = void 0;
-exports.coins = ["btc", "eth", "xmr"];
+exports.coins = ['btc', 'eth', 'xmr'];
